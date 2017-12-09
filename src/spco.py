8b2affc7f93fb43a7801952dffa359511bd0584e
naiaden <louis@naiaden.nl>
2017-12-09 20:51:49 +0100

    First part for testing

--- src/spco.py
+++ src/spco.py
@@ -36,17 +36,19 @@ def split_errors(pattern):
 
     f_max = max(f_a_b_c_d_e, f_a_bc_d_e, f_a_b_cd_e)
     if f_a_b_c_d_e == f_max:
-        return spattern
+        return (False, spattern)
     elif f_a_bc_d_e == f_max:
-        return a_bc_d_e
+        return (True, a_bc_d_e)
     else:
-        return a_b_cd_e
+        return (True, a_b_cd_e)
 
 #p2 = classencoder.buildpattern("een vlinder ui t de")
 #split_errors(p2)
 
 # Compare frequency of (a b cd e) and (a bc d e) with (a b c d e)
 def runon_errors(pattern):
+    something_happened = False
+
     best_f = fr(pattern)
     best_s = ts(pattern)
 
@@ -63,10 +65,11 @@ def runon_errors(pattern):
             p_candidate = classencoder.buildpattern(s_candidate)
             f_candidate = fr(p_candidate)
             if f_candidate > best_f:
+                something_happened = True
                 best_f = f_candidate
                 best_s = s_candidate
 
-    return best_s
+    return (something_happened, best_s)
 
 #p1 = classencoder.buildpattern("een vlinder uit de familie")
 #runon_errors(p1)
@@ -74,24 +77,43 @@ def runon_errors(pattern):
 # archaic, non-word and confusables:
 # Compare frequency of (a b c d e) with (a b X d e)
 def replaceables(pattern):
+    nothing_happened = True
+
     best_f = fr(pattern)
     best_s = ts(pattern)
     c = ts(pattern[2])
 
     for word,count in model.filter(1, size=1):
-        if not word.unknown() and Levenshtein.distance(ts(word), c) < 4:
+        if not word.unknown() and Levenshtein.distance(ts(word), c) < 2:
             a_b_X_d_e = ts(pattern[0:2]) + " " + ts(word) + " " + ts(pattern[3:5])
             p_a_b_X_d_e = classencoder.buildpattern(a_b_X_d_e)
             if fr(p_a_b_X_d_e) > best_f:
+                nothing_happened = False
                 best_f = fr(p_a_b_X_d_e)
                 best_s = a_b_X_d_e
-    return best_s
 
-#p3 = classencoder.buildpattern("een vlinder vit de familie")
-#replaceables(p3)
+    if nothing_happened:
+        for word,count in model.filter(1, size=1):
+            if not word.unknown() and Levenshtein.distance(ts(word), c) < 2:
+                a_b_X_d_e = ts(pattern[1:2]) + " " + ts(word) + " " + ts(pattern[3:4])
+                p_a_b_X_d_e = classencoder.buildpattern(a_b_X_d_e)
+                if fr(p_a_b_X_d_e) > best_f:
+                    nothing_happened = False
+                    best_f = fr(p_a_b_X_d_e)
+                    best_s = a_b_X_d_e
+        if not nothing_happened:
+            return (True, ts(pattern[0]) + " " + best_s + " " + ts(pattern[4]))
+    return (not nothing_happened, best_s)
+
+#p3a = classencoder.buildpattern("een vlinder vit de familie")
+#replaceables(p3a)
+p3b = classencoder.buildpattern("is een vlnder uit familie")
+replaceables(p3b)
 
 # Compare frequency of (a b c d e) with (a b c X d e) (e is ignored)
 def missing_words(pattern):
+    something_happened = False
+
     best_f = fr(pattern)
     best_s = ts(pattern)
 
@@ -101,18 +123,35 @@ def missing_words(pattern):
             p_a_b_c_X_d_e = classencoder.buildpattern(a_b_c_X_d_e)
             f_a_b_c_X_d_e = fr(p_a_b_c_X_d_e)
             if f_a_b_c_X_d_e > best_f:
+                something_happened = True
                 best_f = f_a_b_c_X_d_e
                 best_s = a_b_c_X_d_e
-    return best_s
+    return (something_happened, best_s)
 
 #p4 = classencoder.buildpattern("een vlinder uit familie van")
 #missing_words(p4)
 
 ######################
-## Run the game
+## Set the stage
 
 classencoder = colibricore.ClassEncoder("/home/louis/Data/corpus/small.colibri.cls")
 classdecoder = colibricore.ClassDecoder("/home/louis/Data/corpus/small.colibri.cls")
 options = colibricore.PatternModelOptions(minlength=1, maxlength=5)
 model = colibricore.UnindexedPatternModel()
 model.train("/home/louis/Data/corpus/small.colibri.dat", options)
+
+######################
+## Run the game
+test_sentence = "\"Argyroploce lutosana\" is een vlnder uit familie van de bladrollers (Tortricidae)."
+pts = classencoder.buildpattern(test_sentence, allowunknown=False, autoaddunknown=True)
+
+classencoder.save('/tmp/ce')
+classdecoder = colibricore.ClassDecoder('/tmp/ce')
+
+ts(pts)
+for fivegram in pts.ngrams(5):
+    print("===" + ts(fivegram) + "===")
+    print("RUNON:\t" + str(runon_errors(fivegram)))
+    print("SPLIT:\t" + str(split_errors(fivegram)))
+    print("REPLA:\t" + str(replaceables(fivegram)))
+    print("MISNG:\t" + str(missing_words(fivegram)))
