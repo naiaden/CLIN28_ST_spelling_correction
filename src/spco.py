5bd112554e5a5d0e707de21eebea3b7cfb3da8e6
naiaden <louis@naiaden.nl>
2017-12-10 12:24:33 +0100

    Added corrections for missing word in window

--- src/spco.py
+++ src/spco.py
@@ -21,185 +21,6 @@ def fr(pattern):
 def id(folia_id):
     return folia_id.split(".w.")[0]
 
-######################
-## Correction units
-
-# Compare frequency of (a b c d e) with (a b cd e) and (a bc d e)
-def split_errors(pattern):
-    spattern = ts(pattern)
-    f_a_b_c_d_e = fr(pattern)
-    #print("a b c d e:\t" + str(o_a_b_c_d_e) + "\t" + str(f_a_b_c_d_e) + "\t" + spattern)
-
-    t_spattern = spattern.split(" ")
-
-    a_bc_d_e = t_spattern[0] + " " + t_spattern[1] + t_spattern[2] + " " + t_spattern[3] + " " + t_spattern[4]
-    p_a_bc_d_e = classencoder.buildpattern(a_bc_d_e)
-    f_a_bc_d_e = fr(p_a_bc_d_e)
-    #print("a bc d e:\t" + str(f_a_bc_d_e) + "\t" + a_bc_d_e)
-
-    a_b_cd_e = t_spattern[0] + " " + t_spattern[1] + " " + t_spattern[2] + t_spattern[3] + " " + t_spattern[4]
-    p_a_b_cd_e = classencoder.buildpattern(a_b_cd_e)
-    f_a_b_cd_e = fr(p_a_b_cd_e)
-    #print("a bc d e:\t" + str(f_a_b_cd_e) + "\t" + a_b_cd_e)
-
-    f_max = max(f_a_b_c_d_e, f_a_bc_d_e, f_a_b_cd_e)
-    if f_a_b_c_d_e == f_max:
-        return (False, spattern)
-    elif f_a_bc_d_e == f_max:
-        return (True, a_bc_d_e)
-    else:
-        return (True, a_b_cd_e)
-
-#p2 = classencoder.buildpattern("een vlinder ui t de")
-#split_errors(p2)
-
-# Compare frequency of (a b cd e) and (a bc d e) with (a b c d e)
-def runon_errors(pattern):
-    something_happened = False
-
-    best_f = fr(pattern)
-    best_s = ts(pattern)
-
-    t_pattern = ts(pattern).split(" ")
-    middle = "".join(t_pattern[1:4])
-
-    for x in range(len(middle)):
-        for y in range(len(middle)):
-            candidate = copy.copy(middle)
-            candidate = '{0} {1}'.format(candidate[:x], candidate[x:])
-            candidate = '{0} {1}'.format(candidate[:y], candidate[y:])
-
-            s_candidate = (t_pattern[0] + " " + candidate + " " + t_pattern[4]).replace("  ", " ")
-            p_candidate = classencoder.buildpattern(s_candidate)
-            f_candidate = fr(p_candidate)
-            if f_candidate > best_f:
-                something_happened = True
-                best_f = f_candidate
-                best_s = s_candidate
-
-    return (something_happened and best_s != ts(pattern), best_s)
-
-#p1 = classencoder.buildpattern("een vlinder uit de familie")
-#runon_errors(p1)
-
-# archaic, non-word and confusables:
-# Compare frequency of (a b c d e) with (a b X d e)
-def replaceables(pattern):
-    nothing_happened = True
-
-    best_f = fr(pattern)
-    best_s = ts(pattern)
-    c = ts(pattern[2])
-
-    for word,count in model.filter(1, size=1):
-        if not word.unknown() and Levenshtein.distance(ts(word), c) < 2:
-            a_b_X_d_e = ts(pattern[0:2]) + " " + ts(word) + " " + ts(pattern[3:5])
-            p_a_b_X_d_e = classencoder.buildpattern(a_b_X_d_e)
-            if fr(p_a_b_X_d_e) > best_f:
-                nothing_happened = False
-                best_f = fr(p_a_b_X_d_e)
-                best_s = a_b_X_d_e
-
-    if nothing_happened:
-        for word,count in model.filter(1, size=1):
-            if not word.unknown() and Levenshtein.distance(ts(word), c) < 2:
-                a_b_X_d_e = ts(pattern[1:2]) + " " + ts(word) + " " + ts(pattern[3:4])
-                p_a_b_X_d_e = classencoder.buildpattern(a_b_X_d_e)
-                if fr(p_a_b_X_d_e) > best_f:
-                    nothing_happened = False
-                    best_f = fr(p_a_b_X_d_e)
-                    best_s = a_b_X_d_e
-        if not nothing_happened:
-            best_s = ts(pattern[0]) + " " + best_s + " " + ts(pattern[4])
-    return (not nothing_happened and best_s != ts(pattern), best_s)
-
-#p3a = classencoder.buildpattern("een vlinder vit de familie")
-#replaceables(p3a)
-p3b = classencoder.buildpattern("is een vlnder uit familie")
-replaceables(p3b)
-
-# Compare frequency of (a b c d e) with (a b c X d e) (e is ignored)
-def missing_words(pattern):
-    something_happened = False
-
-    best_f = fr(pattern)
-    best_s = ts(pattern)
-
-    for word,count in model.filter(1, size=1):
-        if not word.unknown():
-            a_b_c_X_d_e = ts(pattern[0:3]) + " " + ts(word) + " " + ts(pattern[3:4])
-            p_a_b_c_X_d_e = classencoder.buildpattern(a_b_c_X_d_e)
-            f_a_b_c_X_d_e = fr(p_a_b_c_X_d_e)
-            if f_a_b_c_X_d_e > best_f:
-                something_happened = True
-                best_f = f_a_b_c_X_d_e
-                best_s = a_b_c_X_d_e
-    return (something_happened and best_s != ts(pattern), best_s)
-
-
-
-#p4 = classencoder.buildpattern("een vlinder uit familie van")
-#missing_words(p4)
-
-######################
-## Set the stage
-
-classencoder = colibricore.ClassEncoder("/home/louis/Data/corpus/small.colibri.cls")
-classdecoder = colibricore.ClassDecoder("/home/louis/Data/corpus/small.colibri.cls")
-options = colibricore.PatternModelOptions(minlength=1, maxlength=5)
-model = colibricore.UnindexedPatternModel()
-model.train("/home/louis/Data/corpus/small.colibri.dat", options)
-
-######################
-## Run the game
-
-test_sentence = "\"Argyroploce lutosana\" is een vlnder uit familie van de bladrollers (Tortricidae)."
-pts = classencoder.buildpattern(test_sentence, allowunknown=False, autoaddunknown=True)
-
-classencoder.save('/tmp/ce')
-classdecoder = colibricore.ClassDecoder('/tmp/ce')
-
-ts(pts)
-change = True
-final_sentence = copy.copy(test_sentence)
-while change:
-    change = False
-    pts = classencoder.buildpattern(final_sentence, allowunknown=False, autoaddunknown=True)
-    print("--------------" + final_sentence + "--------------")
-
-    for fivegram in pts.ngrams(5):
-        print("===" + ts(fivegram) + "===")
-
-        runon = runon_errors(fivegram)
-        change |= runon[0]
-        if runon[0]:
-            final_sentence = final_sentence.replace(ts(fivegram), runon[1])
-            print("RUNON:\t" + "replace " + ts(fivegram) + " with " + runon[1])
-
-        split = split_errors(fivegram)
-        change |= split[0]
-        if split[0]:
-            final_sentence = final_sentence.replace(ts(fivegram), split[1])
-            print("SPLIT:\t" + "replace " + ts(fivegram) + " with " + split[1])
-
-        replace = replaceables(fivegram)
-        change |= replace[0]
-        if replace[0]:
-            final_sentence = final_sentence.replace(ts(fivegram), replace[1])
-            print("REPLA:\t" + "replace " + ts(fivegram) + " with " + replace[1])
-
-        missing = missing_words(fivegram)
-        change |= missing[0]
-        if missing[0]:
-            final_sentence = final_sentence.replace(ts(fivegram[0:4]), missing[1])
-            print("MISNG:\t" + "replace " + ts(fivegram) + " with " + missing[1])
-    print(change)
-
-print(final_sentence)
-
-######################
-## what
-
 def window(iterable, size=2):
     i = iter(iterable)
     win = []
@@ -210,42 +31,9 @@ def window(iterable, size=2):
         win = win[1:] + [e]
         yield win
 
-
-
 ######################
-## Going for the finals['text']
+## Correction units
 
-# add stuff to encoder?
-
-import json
-page1144 = json.load(open('/home/louis/Programming/COCOCLINSPCO/data/test/pagesmall.json'))
-page1144_corrections = page1144['corrections']
-page1144_words = page1144['words']
-
-def missing_words_window(ws):
-    words = [w[1] for w in ws]
-
-    something_happened = False
-    #
-    best_s = classencoder.buildpattern(" ".join(words))
-    best_f = fr(best_s)
-    best_w = ""
-    #
-    for word,count in model.filter(1, size=1):
-        if not word.unknown():
-            a_b_c_X_d_e = " ".join(words[0:3]) + " " + ts(word) + " " + " ".join(words[3:4])
-            p_a_b_c_X_d_e = classencoder.buildpattern(a_b_c_X_d_e)
-            f_a_b_c_X_d_e = fr(p_a_b_c_X_d_e)
-            if f_a_b_c_X_d_e > best_f:
-                something_happened = True
-                best_f = f_a_b_c_X_d_e
-                best_s = a_b_c_X_d_e
-                best_w = ts(word)
-    correction = {}
-    correction['class'] = "missingword"
-    correction['after'] = ws[2][0]
-    correction['text'] = best_w
-    return (something_happened and best_s != " ".join(words), best_s, correction)
 
 # archaic, non-word and confusables:
 # Compare frequency of (a b c d e) with (a b X d e)
@@ -362,6 +150,58 @@ def split_errors_window(ws):
     correction['text'] = best_candidate
     return (something_happened, best_s, correction)
 
+
+######################
+## Set the stage
+
+classencoder = colibricore.ClassEncoder("/home/louis/Data/corpus/small.colibri.cls")
+classdecoder = colibricore.ClassDecoder("/home/louis/Data/corpus/small.colibri.cls")
+options = colibricore.PatternModelOptions(minlength=1, maxlength=5)
+model = colibricore.UnindexedPatternModel()
+model.train("/home/louis/Data/corpus/small.colibri.dat", options)
+
+######################
+## Run the game
+
+
+
+
+
+######################
+## Going for the finals['text']
+
+# add stuff to encoder?
+
+import json
+page1144 = json.load(open('/home/louis/Programming/COCOCLINSPCO/data/test/pagesmall.json'))
+page1144_corrections = page1144['corrections']
+page1144_words = page1144['words']
+
+def missing_words_window(ws):
+    words = [w[1] for w in ws]
+
+    something_happened = False
+    #
+    best_s = classencoder.buildpattern(" ".join(words))
+    best_f = fr(best_s)
+    best_w = ""
+    #
+    for word,count in model.filter(1, size=1):
+        if not word.unknown():
+            a_b_c_X_d_e = " ".join(words[0:3]) + " " + ts(word) + " " + " ".join(words[3:4])
+            p_a_b_c_X_d_e = classencoder.buildpattern(a_b_c_X_d_e)
+            f_a_b_c_X_d_e = fr(p_a_b_c_X_d_e)
+            if f_a_b_c_X_d_e > best_f:
+                something_happened = True
+                best_f = f_a_b_c_X_d_e
+                best_s = a_b_c_X_d_e
+                best_w = ts(word)
+    correction = {}
+    correction['class'] = "missingword"
+    correction['after'] = ws[2][0]
+    correction['text'] = best_w
+    return (something_happened and best_s != " ".join(words), best_s, correction)
+
 def action_in_sentence(sentence, correction):
     print(correction)
     if correction['class'] == "runonerror":
@@ -391,12 +231,25 @@ def action_in_sentence(sentence, correction):
             if id[0] == correction['span'][1]:
                 break
         del sentence[iter]
+    if correction['class'] == "missing":
+        for iter, id in enumerate(sentence):
+            if id[0] == correction['after'][0]:
+                break
+        new_entry = [id[0] + "M",
+                     correction['text'],
+                     classencoder.buildpattern(correction['text'], allowunknown=False, autoaddunknown=True),
+                     item['space'],
+                     item['in']]
+        sentence.insert(iter+1, new_entry)
 
     return sentence
 
 def process(something):
     string_sentence = " ".join([x[1] for x in something])
     wip_sentence = copy.copy(something)
+
+    corrections = []
+
     print("\nSENTENCE: " + string_sentence)
     for w in window(something, 5):
         string_window = " ".join([x[1] for x in w])
@@ -406,6 +259,7 @@ def process(something):
         runon = runon_errors_window(w)
         #change |= split[0]
         if runon[0]:
+            corrections.append(runon[2])
             wip_sentence = action_in_sentence(wip_sentence, runon[2])
             # string_sentence = string_sentence.replace(string_window, runon[1])
             # print("\tRUNON:\t" + "replace [" + string_window + "] with [" + runon[1] + "]")
@@ -414,6 +268,7 @@ def process(something):
         replaceable = replaceables_window(w)
         #change |= split[0]
         if replaceable[0]:
+            corrections.append(replaceable[2])
             wip_sentence = action_in_sentence(wip_sentence, replaceable[2])
             # string_sentence = string_sentence.replace(string_window, replaceable[1])
             # print("\tREPLA:\t" + "replace [" + string_window + "] with [" + replaceable[1] + "]")
@@ -422,22 +277,32 @@ def process(something):
         split = split_errors_window(w)
         #change |= split[0]
         if split[0]:
+            corrections.append(split[2])
             wip_sentence = action_in_sentence(wip_sentence, split[2])
             # string_sentence = string_sentence.replace(string_window, split[1])
             # print("\tSPLIT:\t" + "replace [" + string_window + "] with [" + split[1] + "]")
             # print("\t\t>: " + string_sentence)
+
+        missing = missing_words_window(w)
+        #change |= split[0]
+        if missing[0]:
+            corrections.append(missing[2])
+            wip_sentence = action_in_sentence(wip_sentence, missing[2])
     print("\nRESULT: " + " ".join([x[1] for x in wip_sentence]))
+    return corrections
 
 sentence = []
+page_corrections = []
 current_in = page1144_words[0]['in']
 for item in page1144_words:
     if current_in != item['in']:
         current_in = item['in']
-        process(sentence)
+        page_corrections += process(sentence)
         sentence = []
     sentence.append([item['id'],
                      item['text'],
                      classencoder.buildpattern(item['text'], allowunknown=False, autoaddunknown=True),
                      item['space'],
                      item['in']])
-process(sentence)
+page_corrections += process(sentence)
+print(page_corrections)
